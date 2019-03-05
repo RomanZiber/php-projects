84b3cf92231282b86541092c4a531c0a643bd2f5
RomanZiber <ziberroman@gmail.com>
2019-03-05 21:00:07 +0200

    lesson 4

---
+++ lesson4/main.js
@@ -0,0 +1,43 @@
+"use strict";// for start any code-file(browser)
+
+//комментарий
+
+alert("Hello, World");
+console.log("Hello, console");//комментaрий
+
+/*
+комментарий
+ */
+var message;
+var count = 2;
+var message1 = "fff", label;
+
+message = "Текст сообщения";
+
+alert(message)
+
+// переменными нельзя объявлять начиная с числа + зарезервир. названия ф-ций(var, etc)
+
+//shift+F6 - refactor rename - замена переменных
+
+
+var n = 123;
+var k = 12.345
+
+
+//string
+//null
+
+
+// ОБЪЕКТЫ
+
+
+var  num1, num2, result;
+num1 = +prompt("Введите 1ое число", 0)
+num2 = +prompt("Введите 2ое число", 0)
+
+result = num1 + num2;
+
+alert("Результат сложения:" + result);
+
+
