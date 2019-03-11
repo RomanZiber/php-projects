cf176e6595760e22c22dc5ed1bfc85d5292b5cd3
RomanZiber <ziberroman@gmail.com>
2019-03-11 23:08:32 +0200

    homework4

---
+++ lesson4-homework/task2-homework4.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var c=15;
+var d=2;
+var result = c + d;
+alert(result);
+
+var a = 10;
+var b = 2;
+var c = 5;
+var result = a + b + c;
+alert(result);
+
+var a = 17;
+var b = 10;
+var c = a - b;
+var d = 7;
+var result = c + d;
+alert(result);
+
+var str= "Привет, Мир!";
+alert(str);
+
+var str1 = "Привет, ";
+var str2 = "Мир!";
+var result = str1 + str2;
+alert(result);
+
+var name = "Билл!";
+alert("Привет, " + name);
+
+var age = "100";
+alert("Мне " + age + " лет!");
