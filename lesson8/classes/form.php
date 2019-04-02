85ae955baf781e58ed1bd779a4a26ee15bcac0c1
RomanZiber <ziberroman@gmail.com>
2019-04-02 16:34:34 +0300

    1 commit

--- lesson8/classes/form.php
+++ lesson8/classes/form.php
@@ -1,36 +1,38 @@
 <?php
-
-
-class form
+class Form
 {
     /**
-     * @var array
+     * Fields of form
+     *
+     * @var FormField[]
      */
-private $fields;
-
-/**
- * @var string
- */
-private $method;
-
+    private $fields;
+    /**
+     * @var string
+     */
+    private $method;
     /**
      * @var string
      */
     private $action;
-
-    public function  __construct(string $method, string $action)
+    public function __construct(string $method, string $action)
     {
         $this->method = $method;
         $this->action = $action;
         $this->fields = [];
     }
-
     public function render()
     {
-        $html = ('<form method="%s", action="%s">', $this->method, $this->action);
-        $html .='</form>';
-
+        $html = sprintf('<form method="%s" action="%s">', $this->method, $this->action);
+        // $html = '<form method="' . $this->method . '" action="' . $this->action . '">';
+        foreach ($this->fields as $field) {
+            $html .= $field->render();
+        }
+        $html .= '</form>';
         return $html;
     }
-
+    public function addField(FormField $field)
+    {
+        $this->fields[$field->getName()] = $field;
+    }
 }
--- lesson8/registration.php
+++ lesson8/registration.php
@@ -1,23 +1,20 @@
 <?php
-
-require_once  __DIR__ . 'classes/Form.php';
-
+require_once __DIR__ . '/classes/Form.php';
 $form = new Form('post', 'action.php');
-
 $formGet = new Form('get', 'action.php');
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <title>task1</title>
-    <link rel="stylesheet" href="main.css"
-    </head>
+    <meta charset="utf-8">
+    <title>Form class</title>
+    <link rel="stylesheet" href="main.css">
+</head>
 
 <body>
-    <?= $form->render() ?>
-    <?= $formGet->render() ?>
+<?= $form->render() ?>
+
+<?= $formGet->render() ?>
+
 </body>
 </html>
-
